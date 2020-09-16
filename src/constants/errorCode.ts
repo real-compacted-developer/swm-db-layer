4da4b9ffaf8a11bd2997a3838150e73c20b65e49
combbm <combbm@naver.com>
2020-09-16 22:41:44 +0900

    feat: slide api 응답용 에러코드 추가

--- src/constants/errorCode.ts
+++ src/constants/errorCode.ts
@@ -2,6 +2,7 @@ const ERROR_CODE = {
   QUESTION_NOT_FOUND: '질문을 찾을 수 없습니다.',
   QUESTION_LIKE_NOT_MINUS: '좋아요는 음수가 될 수 없습니다.',
 
+  SLIDE_NOT_FOUND: '슬라이드를 찾을 수 없습니다.',
 };
 
 export default ERROR_CODE;
