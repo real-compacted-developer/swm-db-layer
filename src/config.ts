d0feaf7cde7fc156595db165a6207cc3c94ed07a
combbm <combbm@naver.com>
2020-09-16 20:19:43 +0900

    fix: update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,10 @@ dotenv.config();
 const config = {
   port: parseInt(process.env.PORT!!, 10) || 8080,
   database: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST
+    username: process.env.DB_USERNAME!!,
+    password: process.env.DB_PASSWORD!!,
+    database: process.env.DB_DATABASE!!,
+    host: process.env.DB_HOST!!
   }
 };
 
