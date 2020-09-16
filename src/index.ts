3de0909eaadfd89c01f98a2046b618f5a7f2cb0a
combbm <combbm@naver.com>
2020-09-16 20:27:32 +0900

    fix: move association code to index.ts with db sync code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import db from './database';
+import initAssociation from "./database/association";
 
 export const app = express();
 
+initAssociation();
 db.sequelize.sync().then(() => console.log("Database model connect completed successfully"));
 
 app.use(express.urlencoded({extended: false}));
