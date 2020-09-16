43bf222d3ef7018bcc3ee304db6de45b240bc9db
combbm <combbm@naver.com>
2020-09-16 20:32:18 +0900

    feat: apply cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import db from './database';
@@ -9,5 +10,6 @@ initAssociation();
 db.sequelize.sync().then(() => console.log("Database model connect completed successfully"));
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
