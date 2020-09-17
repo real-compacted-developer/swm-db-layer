94b3582c6ea0a15a7bf377f5f8fee8363d5d7d1f
combbm <combbm@naver.com>
2020-09-17 16:10:47 +0900

    fix: DB 관계 설정 코드 이동
    - fix: database 관련 로그 삭제

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import db from './database';
-import initAssociation from "./database/association";
 import Api from './apis';
 
 export const app = express();
 
-initAssociation();
-db.sequelize.sync().then(() => console.log("Database model connect completed successfully"));
+db.sequelize.sync();
 
 app.use(morgan("dev"));
 app.use(cors());
