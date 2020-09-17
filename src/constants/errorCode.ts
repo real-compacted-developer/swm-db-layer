4f3ed83ef2b3488620ba0623cee24df43caa3bea
combbm <combbm@naver.com>
2020-09-17 13:53:23 +0900

    feat: 사용자, 스터디그룹 관련 에러코드 추가

--- src/constants/errorCode.ts
+++ src/constants/errorCode.ts
@@ -7,6 +7,10 @@ const ERROR_CODE = {
   STUDY_DATA_NOT_FOUND: '스터디 데이터를 찾을 수 없습니다.',
 
   STUDY_GROUP_NOT_FOUND: '스터디그룹을 찾을 수 없습니다.',
+  STUDY_GROUP_ALREADY_EXISTS: '해당 스터디그룹은 이미 존재합니다.',
+
+  USER_NOT_FOUND: '사용자를 찾을 수 없습니다.',
+  USER_ALREADY_EXISTS: '해당 사용자는 이미 존재합니다.',
 };
 
 export default ERROR_CODE;
