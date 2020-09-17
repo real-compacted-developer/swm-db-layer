d7cda87edbc1c06cd2a01dfa81c3f5a9c357409f
combbm <combbm@naver.com>
2020-09-17 14:36:01 +0900

    feat: 스터디 멤버 관련 에러코드 추가

--- src/constants/errorCode.ts
+++ src/constants/errorCode.ts
@@ -11,6 +11,8 @@ const ERROR_CODE = {
 
   USER_NOT_FOUND: '사용자를 찾을 수 없습니다.',
   USER_ALREADY_EXISTS: '해당 사용자는 이미 존재합니다.',
+
+  STUDY_MEMBER_NOT_FOUND: '스터디멤버를 찾을 수 없습니다.'
 };
 
 export default ERROR_CODE;
